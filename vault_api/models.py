0fe58618accdffa3243163dd11915fee99038a72
Brett Abramson <brett.m.abramson@gmail.com>
2023-11-09 14:21:06 -0600

    updated model with djanoUser override, applied migration, registered with admin

--- vault_api/models.py
+++ vault_api/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 # Create your models here.
@@ -8,6 +8,24 @@ class User(AbstractUser):
     spotify_ID = models.CharField(max_length=200, blank=True, null=True)
     albums = models.ManyToManyField("Album", blank=True)
     favorite_genres = models.CharField(max_length=200, blank=True)
+
+    # Overriding the groups and user_permissions fields
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name="groups",
+        blank=True,
+        help_text="Basic User",
+        related_name="custom_user_set",
+        related_query_name="user"
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name="user permissions",
+        blank=True,
+        help_text="Specific Permissions for this user.",
+        related_name="custom_user_set",
+        related_query_name="user"
+    )
 
     def __str__(self):
         return self.username
