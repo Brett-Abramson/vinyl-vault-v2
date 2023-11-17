12c03d81c0822cd1f943e555846a0e31f783ec23
Brett Abramson <brett.m.abramson@gmail.com>
2023-11-17 11:21:58 -0600

    removed files that should be ignored

--- vault_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VaultApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vault_api'
--- vault_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("api/users", views.UserList.as_view(), name="user_list"),
-    path("api/users/<int:pk>", views.UserDetail.as_view(), name="user_detail"),
-    path("api/albums", views.AlbumList.as_view(), name="album_list"),
-    path("api/albums/<int:pk>", views.AlbumDetail.as_view(), name="album_detail"),
-]
