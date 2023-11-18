20d31dcf14c047097511890405de499cf17d250a
Brett Abramson <brett.m.abramson@gmail.com>
2023-11-18 13:24:09 -0600

    added Email settings for aws ses

--- vault_api/urls.py
+++ vault_api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 from .views import ProtectedView
+from django.conf.urls import include
 
 urlpatterns = [
-    path("api/users", views.UserList.as_view(), name="user_list"),
-    path("api/users/<int:pk>", views.UserDetail.as_view(), name="user_detail"),
-    path("api/albums", views.AlbumList.as_view(), name="album_list"),
-    path("api/albums/<int:pk>", views.AlbumDetail.as_view(), name="album_detail"),
-    path("protected/", ProtectedView.as_view(), name="protected")
+
+    # path("api/users", views.UserList.as_view(), name="user_list"),
+    # path("api/users/<int:pk>", views.UserDetail.as_view(), name="user_detail"),
+    # path("api/albums", views.AlbumList.as_view(), name="album_list"),
+    # path("api/albums/<int:pk>", views.AlbumDetail.as_view(), name="album_detail"),
+    # path("protected/", ProtectedView.as_view(), name="protected")
 ]
--- vinyl_vault_v2/urls.py
+++ vinyl_vault_v2/urls.py
@@ -24,11 +24,12 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('', include('vault_api.urls')),
+    # path('', include('vault_api.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('djoser.urls')),
     path('api/', include('djoser.urls.jwt')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
