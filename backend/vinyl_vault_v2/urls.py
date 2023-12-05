deb402263872366a2fde0a95fe5f978706bfa781
Brett Abramson <brett.m.abramson@gmail.com>
2023-12-05 12:44:09 -0600

    api documentation

--- backend/vinyl_vault_v2/urls.py
+++ backend/vinyl_vault_v2/urls.py
@@ -27,7 +27,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title='Vault API',
         default_version='v1',
-        description='Vainyl Vault API',
+        description='Vinyl Vault API',
         terms_of_service='https://www.google.com/policies/terms/',
         contact=openapi.Contact(email=getenv('AWS_SES_FROM_EMAIL')),
         license=openapi.License(name='BSD License')
