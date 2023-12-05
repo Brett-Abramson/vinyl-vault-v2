a77e0af8b0c9f28b6f8521b3d602245496e4ec05
Brett Abramson <brett.m.abramson@gmail.com>
2023-12-05 12:16:14 -0600

    swagger and redoc working

--- backend/vinyl_vault_v2/urls.py
+++ backend/vinyl_vault_v2/urls.py
@@ -15,10 +15,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls import include
+from os import getenv
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Vault API',
+        default_version='v1',
+        description='Vainyl Vault API',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email=getenv('AWS_SES_FROM_EMAIL')),
+        license=openapi.License(name='BSD License')
+    ),
+    public=True,
+    permission_classes=([permissions.AllowAny])
+)
+
 
 urlpatterns = [
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0),
+         name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     # path('', include('vault_api.urls')),
     path('api/v1/', include('vault_api.urls')),
