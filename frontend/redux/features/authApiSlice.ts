f602f98cf89fd0049034efe28029338d7d309b73
Brett Abramson <brett.m.abramson@gmail.com>
2023-12-05 16:40:35 -0600

    auth redux endpoints set up

--- frontend/redux/features/authApiSlice.ts
+++ frontend/redux/features/authApiSlice.ts
@@ -1,9 +1,24 @@
 import { apiSlice } from "../services/apiSlice";
 
+interface User {
+  email: string;
+}
+
+interface SocialAuthArgs {
+  provider: string;
+  state: string;
+  code: string;
+}
+
+interface CreateUserResponse {
+  success: boolean;
+  user: User;
+}
+
 // injecting endpoints here to keep services/apiSlice.ts cleaner
 const authApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    retrieveUser: builder.query({
+    retrieveUser: builder.query<User, void>({
       query: () => "/users/me/",
     }),
     socialAuthenticate: builder.mutation({
@@ -18,22 +33,64 @@ const authApiSlice = apiSlice.injectEndpoints({
         },
       }),
     }),
+    login: builder.mutation({
+      query: ({ email, password }) => ({
+        url: "/jwt/create/",
+        method: "POST",
+        body: { email, password },
+      }),
+    }),
+    register: builder.mutation({
+      query: ({ email, password, re_password }) => ({
+        url: "/jwt/create/",
+        method: "POST",
+        body: { email, password, re_password },
+      }),
+    }),
+    verify: builder.mutation({
+      query: () => ({
+        url: "/jwt/verify/",
+        method: "POST",
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: "/logout/",
+        method: "POST",
+      }),
+    }),
+    activation: builder.mutation({
+      query: ({ uid, token }) => ({
+        url: `/users/activation/`,
+        method: "POST",
+        body: { uid, token },
+      }),
+    }),
+    resetPassword: builder.mutation({
+      query: (email) => ({
+        url: `/users/reset_password/`,
+        method: "POST",
+        body: { email },
+      }),
+    }),
+    resetPasswordConfirm: builder.mutation({
+      query: ({ uid, token, new_password, re_new_password }) => ({
+        url: `/users/reset_password_confirm/`,
+        method: "POST",
+        body: { uid, token, new_password, re_new_password },
+      }),
+    }),
   }),
 });
 
-export const { useRetrieveUserQuery, useSocialAuthenticateMutation } =
-  authApiSlice;
-
-// set up for google before changing to a more universal setUp
-// googleAuthenticate: builder.mutation({
-//   query: ({ state, code }) => ({
-//     url: `/o/google-oauth2/?state=${encodeURIComponent(
-//       state
-//     )}&code=${encodeURIComponent(code)}`,
-//     method: "POST",
-//     header: {
-//       Accept: "application/json",
-//       "Content-Type": "application/x-www-form-urlencoded",
-//     },
-//   }),
-// }),
+export const {
+  useRetrieveUserQuery,
+  useSocialAuthenticateMutation,
+  useLoginMutation,
+  useRegisterMutation,
+  useVerifyMutation,
+  useLogoutMutation,
+  useActivationMutation,
+  useResetPasswordMutation,
+  useResetPasswordConfirmMutation,
+} = authApiSlice;
