bf1841192094dcc9961aa6153e5c63c9311c1146
Brett Abramson <brett.m.abramson@gmail.com>
2023-12-08 13:49:13 -0600

    updated NavBar and is now working, pre Re-Factor

--- frontend/components/common/index.ts
+++ frontend/components/common/index.ts
@@ -1,5 +1,4 @@
-export { default as Navbar } from "./Navbar";
+export { default as Navbar } from "./navigation/Navbar";
 export { default as Footer } from "./Footer";
 export { default as Spinner } from "./Spinner";
 export { default as CustomLink } from "./CustomLink";
-export { default as NavMenu } from "./NavMenu";
