533aa13cdfbc77ed88668959b7dfc60056bda480
Brett Abramson <brett.m.abramson@gmail.com>
2023-12-08 14:16:32 -0600

    moved UserMenu to its own Component

---
+++ frontend/components/common/navigation/UserMenu.tsx
@@ -0,0 +1,55 @@
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+
+interface Props {
+  anchorElUser: any;
+  handleOpenUserMenu: any;
+  handleCloseUserMenu: any;
+}
+
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+const UserMenu = ({
+  anchorElUser,
+  handleOpenUserMenu,
+  handleCloseUserMenu,
+}: Props) => {
+  return (
+    <Box sx={{ flexGrow: 0 }}>
+      <Tooltip title="Open settings">
+        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        sx={{ mt: "45px" }}
+        id="menu-appbar"
+        anchorEl={anchorElUser}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={Boolean(anchorElUser)}
+        onClose={handleCloseUserMenu}
+      >
+        {settings.map((setting) => (
+          <MenuItem key={setting} onClick={handleCloseUserMenu}>
+            <Typography textAlign="center">{setting}</Typography>
+          </MenuItem>
+        ))}
+      </Menu>
+    </Box>
+  );
+};
+
+export default UserMenu;
